c7ab0cb3917aa0a44cfd6c4a66c6b36114944d33
Clemens Stift <clemens@stift.online>
2018-06-22 11:52:40 +0200

    Removed unused dependencies.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from 'ionic-angular';
+
 import { KanbancardComponent } from './kanbancard/kanbancard';
 import { KanbancolumnComponent } from './kanbancolumn/kanbancolumn';
 import { KanbanlaneComponent } from './kanbanlane/kanbanlane';
 import { KanbanboardComponent } from './kanbanboard/kanbanboard';
+
 @NgModule({
 	declarations: [KanbancardComponent,
     KanbancolumnComponent,
     KanbanlaneComponent,
     KanbanboardComponent],
-	imports: [],
-	exports: [KanbancardComponent,
-    KanbancolumnComponent,
-    KanbanlaneComponent,
-    KanbanboardComponent]
+	imports: [
+        BrowserModule,
+        IonicModule.forRoot(ComponentsModule)
+    ],
+    exports: 
+    [
+        KanbancardComponent,
+        KanbancolumnComponent,
+        KanbanlaneComponent,
+        KanbanboardComponent
+    ]
 })
 export class ComponentsModule {}
