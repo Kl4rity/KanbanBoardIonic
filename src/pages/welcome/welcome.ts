a0c5d61195edf00694c1942009c97d5240093191
Clemens Stift <clemens@stift.online>
2018-06-27 10:34:38 +0200

    Added a homepage displaying all the available boards.

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BoardsdataProvider } from '../../providers/boardsdata/boardsdata';
+import { KanbanBoard } from '../../models/KanbanBoard.model';
+import {BoardPage} from '../../pages/board/board';
+
+/**
+ * Generated class for the WelcomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+export class WelcomePage {
+
+  public boards:Array<KanbanBoard>;
+  public pushPage: BoardPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public boardsprovider: BoardsdataProvider) {
+
+  }
+
+  ionViewDidLoad() {
+    this.boards = this.boardsprovider.getBoards();
+  }
+
+  onBoardSelect(board:KanbanBoard){
+    this.navCtrl.push(BoardPage, {"board": board});
+  }
+
+}
