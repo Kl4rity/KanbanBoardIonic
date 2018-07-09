7c2404e4777b746d5829419b264d0800c65838f7
Clemens Stift <clemens@stift.online>
2018-07-09 18:52:09 +0200

    Implemented functionality to edit cards.

--- src/components/kanbancolumn/kanbancolumn.ts
+++ src/components/kanbancolumn/kanbancolumn.ts
@@ -1,5 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { KanbanColumn } from '../../models/KanbanColumn.model';
+import { KanbanCard } from '../../models/KanbanCard.model';
+import { ModalController } from 'ionic-angular';
+import { EditCardPage } from '../../pages/edit-card/edit-card';
+import { KanbanBoard } from '../../models/KanbanBoard.model';
 
 /**
  * Generated class for the KanbancolumnComponent component.
@@ -13,9 +17,10 @@ import { KanbanColumn } from '../../models/KanbanColumn.model';
 })
 export class KanbancolumnComponent {
   @Input() column: KanbanColumn;
+  @Input() board: KanbanBoard;
   @Output() move: EventEmitter<any>;
 
-  constructor() {
+  constructor(public modalCtrl: ModalController) {
     console.log('Hello KanbancolumnComponent Component');
     this.move = new EventEmitter<any>();
   }
@@ -24,5 +29,10 @@ export class KanbancolumnComponent {
     this.move.emit(
       {column: this.column, card: $event.card,  shift: $event.shift}
     );
+  }
+
+  onCardPress(card: KanbanCard){
+    const modal = this.modalCtrl.create(EditCardPage ,{currentCard: card, currentBoard: this.board, column: this.board.columns.indexOf(this.column)});
+    modal.present();
   }
 }
---
+++ src/pages/edit-card/edit-card.ts
@@ -0,0 +1,63 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { KanbanBoard } from '../../models/KanbanBoard.model';
+import { BoardsdataProvider } from '../../providers/boardsdata/boardsdata';
+import { KanbanCard } from '../../models/KanbanCard.model';
+
+/**
+ * Generated class for the EditCardPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-edit-card',
+  templateUrl: 'edit-card.html',
+})
+export class EditCardPage {
+
+  
+  @ViewChild('cardTitleInput') cardTitleInput;
+  @ViewChild('cardContentInput') cardContentInput;
+  @ViewChild('cardTimeInput') cardTimeInput;
+
+  currentBoard: KanbanBoard;
+  columnNumber: number;
+  card: KanbanCard;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public boardsDataProvider: BoardsdataProvider) {
+    this.currentBoard = navParams.get("currentBoard");
+    this.columnNumber = navParams.get("column");
+    this.card = navParams.get("currentCard");
+  }
+
+  ionViewDidLoad() {
+    this.cardTitleInput.value = this.card.title;
+    this.cardContentInput.value = this.card.content;
+    this.cardTimeInput.value = this.card.timeEstimate;
+  }
+
+  onInputEnterPressed(cardTitle: string, cardContent: string, cardTime: number){
+    this.boardsDataProvider.editCard((this.boardsDataProvider.boards.indexOf(this.currentBoard)), this.columnNumber, this.card, cardTitle, cardTime, cardContent);
+    this.navCtrl.pop();
+  }
+
+  onDeleteCard(){
+    this.boardsDataProvider.deleteCard((this.boardsDataProvider.boards.indexOf(this.currentBoard)),this.columnNumber, this.card);
+    this.navCtrl.pop();
+  }
+
+  onShiftFocusToContent(){
+    this.cardContentInput.setFocus();
+  }
+
+  onShiftFocusToTime(){
+    this.cardTimeInput.setFocus();
+  }
+
+  dismiss(){
+    this.navCtrl.pop();
+  }
+}
