1b2a599fea229037d1050c08888336c1d92b628e
Clemens Stift <clemens@stift.online>
2018-07-09 18:53:41 +0200

    fixed bug where column title would throw error

--- src/pages/board/board.ts
+++ src/pages/board/board.ts
@@ -9,9 +9,22 @@ import { KanbanBoard } from '../../models/KanbanBoard.model';
 export class BoardPage {
 
   @Input() board:KanbanBoard;
+  private columnTitle : string;
 
   constructor(public navCtrl: NavController, public navParam: NavParams) {
     this.board = navParam.get("board");
+    console.log(this.board.columns);
+    if(!(this.board.columns[0] === undefined || this.board.columns[0] === null)){
+      this.columnTitle = this.board.columns[0].title;
+    } else {
+      this.columnTitle = "Empty Board";
+    }
   }
 
+  onColumnChange($event){
+    console.log("onColumnChange()");
+    if($event.columnIndex < this.board.columns.length){
+      this.columnTitle = this.board.columns[$event.columnIndex].title;
+    }
+  }
 }
