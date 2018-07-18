527defc7b6247f08588ae14f51b2e32dd2469972
Clemens Stift <clemens@stift.online>
2018-07-18 09:59:35 +0200

    Removed logic for empty board from .ts after having it moved to .html (with an *ngIf)

--- src/pages/board/board.ts
+++ src/pages/board/board.ts
@@ -13,17 +13,14 @@ export class BoardPage {
 
   constructor(public navCtrl: NavController, public navParam: NavParams) {
     this.board = navParam.get("board");
-    console.log(this.board.columns);
     if(!(this.board.columns[0] === undefined || this.board.columns[0] === null)){
       this.columnTitle = this.board.columns[0].title;
-    } else {
-      this.columnTitle = "Empty Board";
     }
   }
 
   onColumnChange($event){
     console.log("onColumnChange()");
-    if($event.columnIndex < this.board.columns.length){
+    if($event.columnIndex < this.board.columns.length && !(this.board.columns[0] === undefined || this.board.columns[0] === null)){
       this.columnTitle = this.board.columns[$event.columnIndex].title;
     }
   }
