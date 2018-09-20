c35acd2d5754cd2300179bf656a64436023c7568
Clemens Stift <clemens@stift.online>
2018-09-20 10:17:26 +0200

    Added karma test runner and jasmine to the project.

---
+++ src/spec/welcome.spec.ts
@@ -0,0 +1,8 @@
+describe('In the file welcome.ts', ()=>{
+    it('performs a useless test', ()=>{
+        expect(1).toEqual(1);
+    });
+    describe('The first nested test', ()=>{
+        pending('write tests for welcome.');
+    });
+});
