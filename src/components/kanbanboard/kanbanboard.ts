68ca1b48e4915da5e54d7d9687c5ce4402a8b51c
Clemens Stift <clemens@stift.online>
2018-09-20 14:28:08 +0200

    Major refactor.

--- src/components/kanbanboard/kanbanboard.ts
+++ src/components/kanbanboard/kanbanboard.ts
@@ -4,7 +4,7 @@ import { ToastController, ModalController, Slides } from 'ionic-angular';
 import { AddcolumnPage } from '../../pages/addcolumn/addcolumn';
 import { AddCardPage } from '../../pages/add-card/add-card';
 import { Vibration } from '@ionic-native/vibration';
-import { Platform } from 'ionic-angular';
+import { isCordova } from '../../shared/isCordova.helper';
 
 
 /**
@@ -26,11 +26,11 @@ export class KanbanboardComponent {
   text: string;
   private vibration:Vibration;
 
-  constructor(public toastCtrl: ToastController, public modalCtrl: ModalController, private platform:Platform) {
+  constructor(public toastCtrl: ToastController, public modalCtrl: ModalController) {
     
     this.columnChange = new EventEmitter<any>();
 
-    if(this.platform.is('cordova')){
+    if(isCordova()){
       this.vibration = new Vibration();
     }
   }
@@ -41,16 +41,16 @@ export class KanbanboardComponent {
     }
   }
 
-    moveCard($event){
-    // Actual moving of data done by the baordModel.
-    let success: boolean = this.board.moveCardToColumn($event.card, $event.column, $event.shift);
-    // Display a Toast message on failure.
-    if(!success){
-        this.showToast("Cannot move Card off Board.");
-        if(this.platform.is('cordova')){
-          this.vibration.vibrate(500);
-        }
+  moveCard($event){
+  // Actual moving of data done by the baordModel.
+  let success: boolean = this.board.moveCardToColumn($event.card, $event.column, $event.shift);
+  // Display a Toast message on failure.
+  if(!success){
+      this.showToast("Cannot move Card off Board.");
+      if(isCordova()){
+        this.vibration.vibrate(500);
       }
+    }
   }
 
   showToast(notification: string) {
@@ -63,6 +63,7 @@ export class KanbanboardComponent {
     toast.present(toast);
   }
 
+  // Why is this on a component? Take it out of here.
   onAddColumn(currentBoard: KanbanBoard){
     const modal = this.modalCtrl.create(AddcolumnPage, {board: currentBoard});
     modal.present();
@@ -74,7 +75,8 @@ export class KanbanboardComponent {
   }
 
   slideChanged(){
-    console.log("slideChanged() :" + this.slides.getActiveIndex());
+    // DEBUG
+    // console.log("slideChanged() :" + this.slides.getActiveIndex());
     this.columnChange.emit({columnIndex: this.slides.getActiveIndex()});
   }
 }
