1aaaf1a493207d54d0933e32c7d59dbcb9381e6b
Clemens Stift <clemens@stift.online>
2018-09-20 14:30:04 +0200

    Added isCordova() helper function

--- src/pages/board/board.ts
+++ src/pages/board/board.ts
@@ -1,51 +1,57 @@
-import { Component, Input } from '@angular/core';
-import { NavController, NavParams, ModalController, Platform } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { KanbanBoard } from '../../models/KanbanBoard.model';
 import { ReorderColumnsPage } from '../reorder-columns/reorder-columns';
 import { Vibration } from '@ionic-native/vibration';
+import { isCordova } from '../../shared/isCordova.helper';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'board.html'
 })
+
 export class BoardPage {
 
-  @Input() board:KanbanBoard;
+  public board: KanbanBoard;
   private columnTitle : string;
   private vibration:Vibration;
 
-  constructor(public navCtrl: NavController, public navParam: NavParams, public modalCtrl: ModalController, private platform:Platform) {
+  constructor(public navCtrl: NavController, public navParam: NavParams, public modalCtrl: ModalController) {
+    
     this.board = navParam.get("board");
 
-    if(this.platform.is('cordova')){
+    if(isCordova()){
       this.vibration = new Vibration();
     }
-
-    if(!(this.board.columns[0] === undefined || this.board.columns[0] === null)){
+    if(this.doBoardsExist()){
       this.columnTitle = this.board.columns[0].title;
     }
-
-
   }
-
-  onColumnChange($event){
-    if($event.columnIndex < this.board.columns.length && !(this.board.columns[0] === undefined || this.board.columns[0] === null)){
-      this.columnTitle = this.board.columns[$event.columnIndex].title;
-    }
-  }
-
   onEditBoard(board: KanbanBoard){
     const modal = this.modalCtrl.create(ReorderColumnsPage, {currentBoard: board});
     modal.present();
   }
-
-  onAttemptedChangeToNonexistant($boardChangeEvent){
-    let boardsNotYetCreated:boolean = (this.board.columns[0] === undefined || this.board.columns[0] === null);
+  onColumnChange($event){
+    this.loadCurrentColumnTitleIntoStatusBarIfItExists($event);
+    this.handleAttemptToSwitchToNonexistantColumn($event);
+  }
+  private handleAttemptToSwitchToNonexistantColumn($boardChangeEvent){
     let isLeftOfBoard:boolean = $boardChangeEvent.columnIndex < 0;
     let isRightOfBoard:boolean = $boardChangeEvent.columnIndex + 1 > this.board.columns.length;
 
-    if(!boardsNotYetCreated && (isLeftOfBoard || isRightOfBoard)){
+    if(this.doBoardsExist() && (isLeftOfBoard || isRightOfBoard)){
       this.vibration.vibrate(500);
     }
+  }
+  private loadCurrentColumnTitleIntoStatusBarIfItExists($boardChangeEvent){
+    let columnChangedToExists: boolean = $boardChangeEvent.columnIndex < this.board.columns.length;
+
+    if(columnChangedToExists && this.doBoardsExist()){
+      this.columnTitle = this.board.columns[$boardChangeEvent.columnIndex].title;
+    }
+  }
+  private doBoardsExist(){
+    return !(this.board.columns[0] === undefined || this.board.columns[0] === null);
   }
 }
