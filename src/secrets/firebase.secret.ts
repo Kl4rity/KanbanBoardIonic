e1fc008666f8f9d6e8c1f2e4747199a7c77ce2cf
Clemens Stift <clemens@stift.online>
2018-09-26 14:03:41 +0200

    Secrets folder not to be synced. API keys in there.

---
+++ src/secrets/firebase.secret.ts
@@ -0,0 +1,8 @@
+export const fireBaseConfig = {
+    apiKey: "AIzaSyBqhGSUOtUmuVLLh3-W41ERs39jd1d5GGc",
+    authDomain: "kanban-d3752.firebaseapp.com",
+    databaseURL: "https://kanban-d3752.firebaseio.com",
+    projectId: "kanban-d3752",
+    storageBucket: "kanban-d3752.appspot.com",
+    messagingSenderId: "302076986119"
+}
